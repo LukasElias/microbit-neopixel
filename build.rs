a4ee20ca2c8768cde5de6cdd55efd563ef8ead93
LukasElias <lukaslundmajland@gmail.com>
2024-08-09 20:57:52 +0200

    Added support for microbit v2

--- build.rs
+++ build.rs
@@ -1,43 +1,15 @@
-use std::process::Command;
-use std::path::Path;
-
 fn main() {
-    // Assemble the assembly file
-    let asm_file = "src/neopixelsend.s";
-    let obj_file = "target/arm-none-eabi/libneopixelsend.o";
-    let staticlib_file = "target/arm-none-eabi/libneopixelsend.a";
-    
-    let assembler_output = Command::new("arm-none-eabi-as")
-        .arg("-march=armv7-m")
-        .arg("-mfloat-abi=hard")
-        .arg("-o")
-        .arg(obj_file)
-        .arg(asm_file)
-        .output()
-        .expect("Failed to execute assembler");
+    #[cfg(feature = "v1")]
+    cc::Build::new()
+        .file("src/neopixelsend.s")
+        .compile("neopixelsend");
 
-    if !assembler_output.status.success() {
-        println!("Assembler output: {:?}", assembler_output.stdout);
-        println!("Assembler error: {:?}", assembler_output.stderr);
-        panic!("Assembler failed");
-    }
+    #[cfg(feature = "v2")]
+    cc::Build::new()
+        .file("src/neopixelsendv2.s")
+        .compile("neopixelsend");
 
-    let archive_output = Command::new("ar")
-        .arg("rcs")
-        .arg(staticlib_file)
-        .arg(obj_file)
-        .output()
-        .expect("Failed to execute archiver");
-
-    if !archive_output.status.success() {
-        println!("Archiver output: {:?}", archive_output.stdout);
-        println!("Archiver error: {:?}", archive_output.stderr);
-        panic!("Archiver failed");
-    }
-
-    // Link the assembly object file
-    println!("cargo:rustc-link-search=native={}", Path::new(&staticlib_file).parent().unwrap().display());
-    println!("cargo:rustc-link-lib=static=neopixelsend");
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=neopixelsend.s");
+    println!("cargo:rerun-if-changed=neopixelsendv2.s");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 extern crate alloc;
 
+use alloc::vec;
 use alloc::vec::Vec;
 use embedded_alloc::Heap;
 use defmt_rtt as _;
@@ -10,6 +11,7 @@ use embedded_hal::digital::OutputPin;
 use microbit::hal::gpio::{
     Pin,
     Output,
+    PushPull,
 };
 
 #[global_allocator]
@@ -22,25 +24,25 @@ extern "C" {
 }
 
 pub struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
 }
 
-pub struct Neopixel<MODE> {
-    pin: Pin<Output<MODE>>,
+pub struct Neopixel {
+    pin: Pin<Output<PushPull>>,
     num_pixels: u16,
     data: Vec<u8>,
 }
 
-impl<MODE> Neopixel<MODE> {
-    pub fn new(pin: Pin<Output<MODE>>, num_pixels: u16) -> Self {
+impl Neopixel {
+    pub fn new(pin: Pin<Output<PushPull>>, num_pixels: u16) -> Self {
 
         Self {
             pin,
             num_pixels,
             data: {
-                let mut data_buffer: Vec<u8> = vec![0; num_pixels];
+                let mut data_buffer: Vec<u8> = vec![0; num_pixels as usize];
 
                 for _ in 0..num_pixels * BYTES_PER_PIXEL as u16 {
                     data_buffer.push(0);
@@ -51,13 +53,19 @@ impl<MODE> Neopixel<MODE> {
         }
     }
 
-    pub fn set_pixel(&mut self, pixel_index: u16, color: Color) {
+    pub fn set_pixel(&mut self, pixel_index: u16, color: &Color) {
         let byte_index = pixel_index as usize * BYTES_PER_PIXEL as usize;
 
         // The reason green is first is because the neopixel strip is grb not rgb
         self.data[byte_index] = color.green;
         self.data[byte_index + 1] = color.red;
         self.data[byte_index + 2] = color.blue;
+    }
+
+    pub fn clear_color(&mut self, color: &Color) {
+        for pixel in 0..self.num_pixels {
+            self.set_pixel(pixel, color);
+        }
     }
     
     pub fn show(&mut self) {
